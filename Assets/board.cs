03114419a80f87b70bc87dafb1e91a832aca7e64
catiejojo <catiejo.pidel@gmail.com>
2016-04-25 18:08:04 -0500

    je vais le pusher

---
+++ Assets/board.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class board : MonoBehaviour {
+
+	public int width = 4;
+	public int height = 10;
+	public note[,] board;
+
+	// Use this for initialization
+	void Start () {
+		board = new note[width, height];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
