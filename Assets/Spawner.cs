fa226b455fcf05b8dc8a7b07a83013b52bf2ba3a
catiejojo <catiejo.pidel@gmail.com>
2018-02-21 16:22:06 +0100

    assets

---
+++ Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] options;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void spawnNext() {
+		// Random Index
+		int i = Random.Range(0, options.Length);
+
+		// Spawn Group at current Position
+		Instantiate(options[i],
+			transform.position,
+			Quaternion.identity);
+	}
+}
--- Assets/board.cs
+++ Assets/board.cs
@@ -5,17 +5,45 @@ public class board : MonoBehaviour {
 
 	public int width = 4;
 	public int height = 10;
-	public note[,] board;
+	public note note;
+	public note[,] notes;
+	public int cellSize = 40;
 
 	// Use this for initialization
 	void Start () {
-		board = new note[width, height];
-	
+		notes = new note[width, height];
+		notes [0, height - 1] = note;
+		StartCoroutine (UpdateNotes ());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator UpdateNotes () {
+		for (;;) {
+			Fall ();
+			Display ();
+			yield return new WaitForSeconds (1);
+		}
 	}
 
+	void Fall () {
+		for (int i = 0; i < width; i++) {
+			for (int j = 1; j < height; j++) {
+				if (!notes [i, j - 1]) {
+					notes [i, j - 1] = notes [i, j];
+					notes [i, j] = null;
+				}
+			}
+		}
+	}
+
+	void Display () {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				var curNote = notes [i, j];
+				if (curNote) {
+					curNote.transform.localPosition = new Vector3 (i, j, 0) * cellSize;
+				}
+			}
+		}
+	}
 }
